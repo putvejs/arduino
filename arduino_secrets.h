4b8d5a8e7501aeb23deb4055c53c6b8ff5b76779
putvejs <dzerins@gmail.com>
2021-07-07 15:51:02 +0300

    Commit

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,9 +1,9 @@
-  GNU nano 5.8                                                                                                                   arduino_secrets.h                                                                                                                             
+  GNU nano 4.8                                                                                                        weatherStation_BME280/arduino_secrets.h                                                                                                                  
 #define SECRET_PASS ""
 #define SECRET_SSID ""
-#define MQTT_SERVER_ID ""
-#define MQTT_USER_ID ""
-#define MQTT_PASSWORD_ID ""
+#define MQTT_SERVER_ID "srv.op"
+#define MQTT_USER_ID "putvejs"
+#define MQTT_PASSWORD_ID "Downtime"
 
-#define DEVICE_ID ""
-#define CLIENT_ID ""
+#define DEVICE_ID "bme280"
+#define CLIENT_ID "dev01"
