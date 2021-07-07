4df6406cae08c51321d94490d7e6995c492c6512
putvejs <37452007+putvejs@users.noreply.github.com>
2021-07-07 15:57:53 +0300

    Update weatherStation_BME280.ino

--- weatherStation_BME280/weatherStation_BME280.ino
+++ weatherStation_BME280/weatherStation_BME280.ino
@@ -40,7 +40,7 @@ WiFiClient espClient;
 
 //mqtt definition
 
-String MQTT_TOPIC_STATE = CLIENT_ID+"/"+DEVICE_ID+"/status";
+const char *MQTT_TOPIC_STATE = CLIENT_ID+"/"+DEVICE_ID+"/status";
 
 //#define MQTT_TOPIC_STATE 
 #define MQTT_PUBLISH_DELAY 60000
