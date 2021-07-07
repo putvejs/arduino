29f426c6e36f32034ea249185259f284462c146c
putvejs <37452007+putvejs@users.noreply.github.com>
2021-07-07 14:00:52 +0300

    Create arduino_secrets.h

---
+++ arduino_secrets.h
@@ -0,0 +1,9 @@
+  GNU nano 5.8                                                                                                                   arduino_secrets.h                                                                                                                             
+#define SECRET_PASS ""
+#define SECRET_SSID ""
+#define MQTT_SERVER_ID ""
+#define MQTT_USER_ID ""
+#define MQTT_PASSWORD_ID ""
+
+#define DEVICE_ID ""
+#define CLIENT_ID ""
