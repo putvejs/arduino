00730b4156b983b8b456af484343d36eacd75913
putvejs <37452007+putvejs@users.noreply.github.com>
2021-07-08 13:21:59 +0300

    Delete arduino_secrets.h

--- arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-  GNU nano 4.8                                                                                                        weatherStation_BME280/arduino_secrets.h                                                                                                                  
-#define SECRET_PASS ""
-#define SECRET_SSID ""
-#define MQTT_SERVER_ID "srv.op"
-#define MQTT_USER_ID "putvejs"
-#define MQTT_PASSWORD_ID "Downtime"
-
-#define DEVICE_ID "bme280"
-#define CLIENT_ID "dev01"
